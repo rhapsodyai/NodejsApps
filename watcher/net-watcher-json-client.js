ff4a3ab6747cf5dfff98dc75468b960ce539cb34
rhapsodyai <rhapsody.ai@gmail.com>
2017-07-12 01:28:32 -0700

    added ldj client

--- watcher/net-watcher-json-client.js
+++ watcher/net-watcher-json-client.js
@@ -1,10 +1,12 @@
+//run with node --harmony net-watcher-json-client.js
+
 "use strict";
 const net = require('net'), client = net.connect({port: 5432});
 client.on('data', function(data) {
 	let message = JSON.parse(data);
-	if(message.type === 'watching') {
+	if (message.type === 'watching') {
 		console.log("Now watching: " + message.file);
-	} else if(message.type === 'changed') {
+	} else if (message.type === 'changed') {
 		let date = new Date(message.timestamp);
 		console.log("File '" + message.file + "' changed at " + date);
 	} else {
